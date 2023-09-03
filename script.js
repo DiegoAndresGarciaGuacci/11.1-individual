0394513e8ee0c09538067551f5a4fc883f0563bd
DiegoAndresGarciaGuacci <dagarciaguacci1@gmail.com>
2023-09-03 18:24:37 -0300

    1
    1

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const botonApi = document.getElementById('botonApi');
-const infoApi = document.getElementById('infoApi');
-const expBase = document.getElementById('expBase');
 
-const llamarApi = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/dragonite')
-    .then(res => res.json())
-    .then(data => {
-        infoApi.innerText = JSON.stringify(data);
-        expBase.innerText = `EXP BASE: ${data.base_experience}`; 
-    })
-    .catch(e => console.error(`Error en el sistema: ${e}`));
-}
-
-botonApi.addEventListener('click', llamarApi);
+        function getDragoniteData() {
+            
+            fetch('https://pokeapi.co/api/v2/pokemon/dragonite')
+                .then(response => response.json())
+                .then(data => {
+                   
+                    const dragoniteData = `
+                        <p><strong>Nombre:</strong> ${data.name}</p>
+                        <p><strong>Altura:</strong> ${data.height / 10} metros</p>
+                        <p><strong>Peso:</strong> ${data.weight / 10} kg</p>
+                        <p><strong>Tipo:</strong> ${data.types.map(type => type.type.name).join(', ')}</p>
+                    `;
+                    document.getElementById('dragoniteData').innerHTML = dragoniteData;
+                })
+                .catch(error => {
+                    console.error('Error al obtener los datos de Dragonite:', error);
+                });
+        }
