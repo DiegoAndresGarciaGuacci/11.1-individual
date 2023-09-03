6350539ad76621dabfe61603cdc1bf8b9f676f08
DiegoAndresGarciaGuacci <dagarciaguacci1@gmail.com>
2023-09-03 16:06:37 -0300

    1
    1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botonApi = document.getElementById('botonApi');
-const infoApi = document.getElementById('infoApi'); // Cambié el ID para evitar duplicados
+const infoApi = document.getElementById('infoApi');
 const expBase = document.getElementById('expBase');
 
 const llamarApi = () => {
@@ -7,9 +7,9 @@ const llamarApi = () => {
     .then(res => res.json())
     .then(data => {
         infoApi.innerText = JSON.stringify(data);
-        expBase.innerText = `EXP BASE: ${data.base_experience}`; // Corregí la propiedad de "expBase"
+        expBase.innerText = `EXP BASE: ${data.base_experience}`; 
     })
-    .catch(e => console.error(`Error en el sistema: ${e}`)); // Corregí la forma de imprimir el error
+    .catch(e => console.error(`Error en el sistema: ${e}`));
 }
 
 botonApi.addEventListener('click', llamarApi);
